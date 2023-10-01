7e365699bbe79bc4d2213508cc44ea11de10071f
PU JA <pujatangka@gmail.com>
2023-10-01 19:19:19 +0600

    nxt

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,29 +4,42 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
     const withdrawField = document.getElementById('withdraw-field');
     const withdrawAmountString = withdrawField.value;
     const newWithdrawAmount=parseInt(withdrawAmountString);
+   
+   
+   
     //step-3
     const withdrawTotalElement = document.getElementById('withdraw-total');
      const withdrawTotalstring =withdrawTotalElement.innerText;
         const newwithdrawTotal= parseInt(withdrawTotalstring);
-    //step- 4
     
-    const addWithdraw = newWithdrawAmount + newwithdrawTotal;
-    //step-5
-    withdrawTotalElement.innerText= addWithdraw;
-
+     // step- 7 // if ar niche thakle input field khali hoy na
+     withdrawField.value = '';
 
     ////step - 6 
 
         const withdrawBalance = document.getElementById('balance-total');
         const withdrawBalanceString = withdrawBalance.innerText;
         const newWithdrawBalance = parseInt(withdrawBalanceString);
+
+       if(newWithdrawAmount>newWithdrawBalance){
+        alert('fuck you')
+        return; // aikhne return na dile balance ar calculation neg asbe
+       }
+
+       
+
+         //step- 4 // if ar condition ar jonno step 4 k nike ana hoise naile withdaraw te tk show kore 
+    
+    const addWithdraw = newWithdrawAmount + newwithdrawTotal;
+    
+    //step-5
+    withdrawTotalElement.innerText= addWithdraw;
+
+          
+
         const balance2= newWithdrawBalance - newWithdrawAmount;
         
         withdrawBalance.innerText= balance2;
-        // step- 7
-          withdrawField.value = '';
-
-
-
+       
 
 })
